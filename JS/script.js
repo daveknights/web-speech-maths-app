dc2417936a4840f6be394a1861838dd0a6b588fe
David Knights <david.knights@vcars.co.uk>
2022-02-12 10:18:54 +0000

    Provide subtract & divide options

--- JS/script.js
+++ JS/script.js
@@ -12,9 +12,15 @@ const answerText = document.querySelector('.answer');
 const answerBtn = document.querySelector('.answer-btn');
 const nextBtn = document.querySelector('.next-btn');
 
+const divisions = [[[8,6,4], 2], [[9,6], 3], [[8], 4]];
 const digits = [2,3,4,5,6,7,8,9];
-const operators = ['+', '*'];
+const operators = ['+', '*', '-', '/'];
 let answer = '';
+
+const displaySymbols = {
+    '*': '&times;',
+    '/': '&#247;',
+}
 
 const checkAnswer = guess => {
     if (guess === answer) {
@@ -34,20 +40,38 @@ recognition.onresult = event => {
 
 recognition.onspeechend = () => recognition.stop();
 
-const shuffle = arrToShuffle => arrToShuffle
+const shuffle = (arrToShuffle, minus=false) => {
+    const length = minus === true ? 2 : 1;
+
+    return arrToShuffle
     .map(value => ({ value, sort: Math.random() }))
     .sort((a, b) => a.sort - b.sort)
-    .map(({ value }) => value).slice(0,1);
+    .map(({ value }) => value).slice(0,length);
+};
 
 const getQuestion = () => {
     form.classList.remove('correct', 'wrong');
     answerText.textContent = '';
 
-    firstNum.value = shuffle(digits);
-    secondNum.value = shuffle(digits);
     const symbol = shuffle(operators);
 
-    operator.innerHTML = symbol[0] === '*' ? '&times;' :  symbol;
+    switch (symbol[0]) {
+        case '-':
+            nums = shuffle(digits, true).sort().reverse();
+            firstNum.value = nums[0];
+            secondNum.value = nums[1];
+            break;
+        case '/':
+            const divisionNums = shuffle(divisions);
+            firstNum.value = shuffle(divisionNums[0][0]);
+            secondNum.value = divisionNums[0][1];
+            break;
+        default:
+            firstNum.value = shuffle(digits);
+            secondNum.value = shuffle(digits);
+    }
+
+    operator.innerHTML = symbol[0] in displaySymbols ? displaySymbols[symbol[0]] :  symbol;
 
     answer = eval(`${firstNum.value}${symbol}${secondNum.value}`).toString();
 };
